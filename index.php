c47ceb313363531e91334c00ce3686141b9c7143
marsel945 <rewoherson519@gmail.com>
2023-08-22 20:23:10 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="d-flex justify-content-center">
+
+        <div class="card col-8">
+            <div class="card-header">
+                Biodata
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Marselinus Harson Rewo</li>
+                <li class="list-group-item">Universitas Pendidikan Ganesha</li>
+                <li class="list-group-item">Sistem Informasi</li>
+            </ul>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
